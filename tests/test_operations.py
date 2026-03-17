11e5776220dc7373dd72c9a7eea5a417ef2274d9
p <pushya.singh@somaiya.edu>
2026-03-17 16:34:39 +0530

    update

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -4,6 +4,7 @@ from src.math_operations import add,sub,mul
 def test_add(): 
     assert add(2,3)==5 #assert add(1,1)==2
     assert add(-1,1)==0
+    assert add(4,5)==0
     
 def test_sub():
     assert sub(5,3)==2 #assert sub(2,1)==1
@@ -15,3 +16,4 @@ def test_mul():
     assert mul(2,3)==6 #assert mul(2,2)==4
     assert mul(-1,1)==-1
     assert mul(0,5)==0
+
